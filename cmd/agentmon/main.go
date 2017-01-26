1b87ab0d40e13fa27edf515be6c589afa0a4c6cb
Andrew Gwozdziewycz <apg@heroku.com>
2017-01-26 11:40:21 -0800

    Initial commit.

---
+++ cmd/agentmon/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/heroku/agentmon"
+)
+
+var showVersion = flag.Bool("version", false, "print version string")
+
+func main() {
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("agentmon/%v (built w/%s)\n", agentmon.VERSION, runtime.Version())
+	}
+
+	fmt.Println("Hello, world")
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM)
+
+	// TODO: Do something with a final flush on TERM.
+
+}
---
+++ version.go
@@ -0,0 +1,4 @@
+package agentmon
+
+// LINK time populated based on git tag.
+var VERSION = "0.0.1"
