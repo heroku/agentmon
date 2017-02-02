9289cf99a310f2da930782b1d4c08b1aef318e88
Andrew Gwozdziewycz <apg@heroku.com>
2017-02-02 15:12:51 -0800

    Add prometheus poller.
    Hold on to your fedoras! That's a lot of deps!

---
+++ prom/poller.go
@@ -0,0 +1,196 @@
+package prom
+
+import (
+	"context"
+	"io"
+	"log"
+	"mime"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	ag "github.com/heroku/agentmon"
+	"github.com/matttproud/golang_protobuf_extensions/pbutil"
+	"github.com/prometheus/common/expfmt"
+
+	dto "github.com/prometheus/client_model/go"
+)
+
+const (
+	defaultPollInterval = 5 * time.Second
+
+	defaultAcceptHeader = `application/vnd.google.protobuf;proto=io.prometheus.client.MetricFamily;encoding=delimited;q=0.7,text/plain;version=0.0.4;q=0.3`
+	promMediaType       = "application/vnd.google.protobuf"
+	promEncoding        = "delimited"
+	promProto           = "io.prometheus.client.MetricFamily"
+)
+
+type PrometheusConfig struct {
+	URL          *url.URL
+	Interval     time.Duration
+	AcceptHeader string
+}
+
+type PrometheusPoller struct {
+	Config PrometheusConfig
+	Inbox  chan *ag.Measurement
+}
+
+func (p PrometheusPoller) Poll(ctx context.Context) {
+	if p.Config.Interval == 0 {
+		p.Config.Interval = defaultPollInterval
+	}
+	go p.pollLoop(ctx)
+}
+
+func (p PrometheusPoller) pollLoop(ctx context.Context) {
+	t := time.NewTicker(p.Config.Interval)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-t.C:
+			ch := make(chan *dto.MetricFamily, 1024)
+			tctx, cancel := context.WithTimeout(ctx, p.Config.Interval)
+			go p.fetchFamilies(tctx, ch)
+			p.sync(tctx, ch)
+			cancel()
+		}
+	}
+}
+
+func (p PrometheusPoller) sync(ctx context.Context, ch <-chan *dto.MetricFamily) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case fam := <-ch:
+			if ms, ok := familyToMeasurements(fam); ok {
+				for _, m := range ms {
+					// TODO: Probably don't want to block on this, drop instead.
+					p.Inbox <- m
+				}
+			}
+		}
+	}
+}
+
+func (p PrometheusPoller) fetchFamilies(ctx context.Context, ch chan<- *dto.MetricFamily) {
+	u := p.Config.URL.String()
+	req, err := http.NewRequest(http.MethodGet, u, nil)
+	if err != nil {
+		log.Fatalf("creating GET request for URL %q failed: %s", u, err)
+	}
+
+	req = req.WithContext(ctx)
+	if p.Config.AcceptHeader == "" {
+		req.Header.Add("Accept", defaultAcceptHeader)
+	} else {
+		req.Header.Add("Accept", p.Config.AcceptHeader)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("executing GET request for URL %q failed: %s", u, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("GET request for URL %q returned HTTP status %s", u, resp.Status)
+	}
+
+	mtype, params, err := mime.ParseMediaType(resp.Header.Get("Content-Type"))
+	if err == nil && mtype == promMediaType &&
+		params["encoding"] == promEncoding &&
+		params["proto"] == promProto {
+
+		for {
+			mf := &dto.MetricFamily{}
+			if _, err := pbutil.ReadDelimited(resp.Body, mf); err != nil {
+				if err == io.EOF {
+					break
+				}
+				log.Fatalln("reading metric family protocol buffer failed:", err)
+			}
+			ch <- mf
+		}
+	} else {
+		// We could do further content-type checks here, but the
+		// fallback for now will anyway be the text format
+		// version 0.0.4, so just go for it and see if it works.
+		var parser expfmt.TextParser
+
+		metricFamilies, err := parser.TextToMetricFamilies(resp.Body)
+
+		if err != nil {
+			log.Fatalln("reading text format failed:", err)
+		}
+		for _, mf := range metricFamilies {
+			ch <- mf
+		}
+	}
+}
+
+func familyToMeasurements(mf *dto.MetricFamily) (out []*ag.Measurement, ok bool) {
+	name := mf.GetName()
+	switch mf.GetType() {
+	case dto.MetricType_GAUGE:
+		for _, m := range mf.Metric {
+			out = append(out, &ag.Measurement{
+				Name:      name + suffixFor(m),
+				Timestamp: msToTime(m.GetTimestampMs()),
+				Type:      "g",
+				Value:     getValue(m),
+				Sample:    1.0,
+			})
+			ok = true
+		}
+	case dto.MetricType_COUNTER:
+		for _, m := range mf.Metric {
+			out = append(out, &ag.Measurement{
+				Name:      name + suffixFor(m),
+				Timestamp: msToTime(m.GetTimestampMs()),
+				Type:      "c",
+				Value:     getValue(m),
+				Sample:    1.0,
+			})
+			ok = true
+		}
+	}
+	return
+}
+
+func msToTime(ms int64) time.Time {
+	secs := ms / 1000
+	ns := time.Duration(ms%1000) * time.Millisecond
+	return time.Unix(secs, int64(ns)).UTC()
+}
+
+func getValue(m *dto.Metric) float64 {
+	if m.Gauge != nil {
+		return m.GetGauge().GetValue()
+	}
+	if m.Counter != nil {
+		return m.GetCounter().GetValue()
+	}
+	if m.Untyped != nil {
+		return m.GetUntyped().GetValue()
+	}
+	return 0
+}
+
+// suffixFor returns a dot separated string of `label_values`.
+func suffixFor(m *dto.Metric) string {
+	result := make([]string, 0, len(m.Label))
+
+	for _, lp := range m.Label {
+		result = append(result, lp.GetName()+"_"+lp.GetValue())
+	}
+
+	if len(result) == 0 {
+		return ""
+	}
+	return "." + strings.Join(result, ".")
+}
---
+++ prom/poller_test.go
@@ -0,0 +1,223 @@
+package prom
+
+import (
+	"bytes"
+	"context"
+	"log"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"time"
+
+	am "github.com/heroku/agentmon"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	dto "github.com/prometheus/client_model/go"
+)
+
+func setup() (*url.URL, map[string]float64, func()) {
+	reg := prometheus.NewRegistry()
+
+	counterVec := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name:        "some_counter",
+			Help:        "counter help",
+			ConstLabels: prometheus.Labels{"type": "http"},
+		},
+		[]string{"code"},
+	)
+	counterVec.WithLabelValues("200").Inc()
+	counterVec.WithLabelValues("500").Inc()
+	reg.MustRegister(counterVec)
+
+	gaugeVec := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name:        "some_gauge",
+			Help:        "gauge help",
+			ConstLabels: prometheus.Labels{"type": "temperature"},
+		},
+		[]string{"location"},
+	)
+	gaugeVec.WithLabelValues("office").Add(75)
+	gaugeVec.WithLabelValues("kitchen").Add(76)
+	reg.MustRegister(gaugeVec)
+
+	expectations := map[string]float64{
+		"some_counter.code_200.type_http":              1,
+		"some_counter.code_500.type_http":              1,
+		"some_gauge.location_office.type_temperature":  75,
+		"some_gauge.location_kitchen.type_temperature": 76,
+	}
+
+	buf := &bytes.Buffer{}
+	logger := log.New(buf, "", 0)
+
+	handler := promhttp.HandlerFor(reg, promhttp.HandlerOpts{
+		ErrorLog:      logger,
+		ErrorHandling: promhttp.HTTPErrorOnError,
+	})
+
+	server := httptest.NewServer(handler)
+	u, _ := url.Parse(server.URL)
+	return u, expectations, func() {
+		server.Close()
+	}
+}
+
+func TestPromPoller(t *testing.T) {
+	acceptHeaders := []string{
+		`text/plain; version=0.0.4`,
+		`application/vnd.google.protobuf; proto=io.prometheus.client.MetricFamily; encoding=delimited`,
+		`application/vnd.google.protobuf; proto=io.prometheus.client.MetricFamily; encoding=delimited; q=0.7,text/plain; version=0.0.4; q=0.3`,
+	}
+
+	u, exp, teardown := setup()
+	defer teardown()
+
+	for _, ah := range acceptHeaders {
+		t.Run(ah, func(t *testing.T) {
+			expCopy := make(map[string]float64)
+			for k, v := range exp {
+				expCopy[k] = v
+			}
+			testPollerForType(t, u, expCopy, ah)
+		})
+	}
+}
+
+func testPollerForType(t *testing.T, u *url.URL, exp map[string]float64, acceptHeader string) {
+	config := PrometheusConfig{
+		URL:          u,
+		Interval:     5 * time.Millisecond,
+		AcceptHeader: acceptHeader,
+	}
+
+	in := make(chan *am.Measurement, 4)
+	poller := PrometheusPoller{Config: config, Inbox: in}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	poller.Poll(ctx)
+
+	// Basically, we want a single
+	timeout := time.After(9 * time.Millisecond)
+
+	for {
+		select {
+		case m := <-in:
+			if val, ok := exp[m.Name]; !ok {
+				t.Fatalf("Received measurement for unexpected metric %s", m.Name)
+			} else if val != m.Value {
+				t.Fatalf("Expected value=%f, got=%f", val, m.Value)
+			}
+			delete(exp, m.Name)
+		case <-timeout:
+			cancel()
+			if len(exp) > 0 {
+				t.Fatalf("Expectations left unsatisfied: %+v", exp)
+			}
+			return
+		}
+	}
+}
+
+func fakeMetricFamily() (*dto.MetricFamily, []*am.Measurement) {
+	sc := "some_counter"
+	mt := dto.MetricType_COUNTER
+	t00 := "200"
+	f00 := "500"
+	code := "code"
+	typ := "type"
+	htp := "http"
+	one := float64(1)
+
+	return &dto.MetricFamily{
+			Name: &sc,
+			Type: &mt,
+			Metric: []*dto.Metric{
+				{
+					Label: []*dto.LabelPair{
+						&dto.LabelPair{Name: &code, Value: &t00},
+						&dto.LabelPair{Name: &typ, Value: &htp},
+					},
+					Counter: &dto.Counter{Value: &one},
+				},
+				{
+					Label: []*dto.LabelPair{
+						&dto.LabelPair{Name: &code, Value: &f00},
+						&dto.LabelPair{Name: &typ, Value: &htp},
+					},
+					Counter: &dto.Counter{Value: &one},
+				},
+			},
+		}, []*am.Measurement{
+			{
+				Name:  "some_counter.code_200.type_http",
+				Value: 1,
+				Type:  "c",
+			},
+			{
+				Name:  "some_counter.code_500.type_http",
+				Value: 1,
+				Type:  "c",
+			},
+		}
+
+}
+
+func TestPollerSync(t *testing.T) {
+	mf, expected := fakeMetricFamily()
+
+	inbox := make(chan *am.Measurement, 2)
+	poller := PrometheusPoller{Inbox: inbox}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	ch := make(chan *dto.MetricFamily, 1)
+	go poller.sync(ctx, ch)
+	ch <- mf
+
+	ei := 0
+
+loop:
+	for {
+		select {
+		case m := <-inbox:
+			if expected[ei].Name != m.Name {
+				t.Errorf("Expected name=%q got=%q", expected[ei].Name, m.Name)
+			}
+			if expected[ei].Value != m.Value {
+				t.Errorf("Expected name=%f got=%f", expected[ei].Value, m.Value)
+			}
+			if expected[ei].Type != m.Type {
+				t.Errorf("Expected type=%q got=%q", expected[ei].Type, m.Type)
+			}
+			ei++
+
+		case <-time.After(1 * time.Millisecond):
+			if len(expected) != ei {
+				t.Fatalf("Expected to receive %d measurements, found %d", len(expected), ei)
+			}
+			break loop
+		}
+	}
+
+	cancel()
+}
+
+func TestPollerSyncCancel(t *testing.T) {
+	mf, _ := fakeMetricFamily()
+
+	inbox := make(chan *am.Measurement, 2)
+	poller := PrometheusPoller{Inbox: inbox}
+	ctx, cancel := context.WithCancel(context.Background())
+	ch := make(chan *dto.MetricFamily, 1)
+	cancel()
+	go poller.sync(ctx, ch)
+	ch <- mf
+
+	select {
+	case m := <-inbox:
+		t.Fatalf("Got something %+v, expected nothing", m)
+	case <-time.After(1 * time.Millisecond):
+	}
+}
