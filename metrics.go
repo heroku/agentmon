ad23aca3410f69705686e3004bb23c7846402294
Andrew Gwozdziewycz <apg@heroku.com>
2017-02-24 14:14:17 -0800

    Add simple metrics serialization.

--- metrics.go
+++ metrics.go
@@ -10,3 +10,28 @@ type Measurement struct {
 	Sample    float32
 	Modifier  string
 }
+
+type MeasurementSet struct {
+	Counters map[string]float64 `json:"counters,omitempty"`
+	Gauges   map[string]float64 `json:"gauges,omitempty"`
+}
+
+func NewMeasurementSet() *MeasurementSet {
+	return &MeasurementSet{
+		Counters: make(map[string]float64),
+		Gauges:   make(map[string]float64),
+	}
+}
+
+func (ms *MeasurementSet) Update(m *Measurement) {
+	switch m.Type {
+	case "c":
+		ms.Counters[m.Name] += m.Value * (1 / float64(m.Sample))
+	case "g":
+		ms.Gauges[m.Name] += m.Value * (1 / float64(m.Sample))
+	}
+}
+
+func (ms *MeasurementSet) Len() int {
+	return len(ms.Counters) + len(ms.Gauges)
+}
