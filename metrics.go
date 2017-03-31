c9a469612a83606b753628991b012817892cfe67
Andrew Gwozdziewycz <apg@heroku.com>
2017-03-31 14:47:39 -0700

    Make the base package golint clean.

--- metrics.go
+++ metrics.go
@@ -2,26 +2,53 @@ package agentmon
 
 import "time"
 
+// MetricType represents the type of metric, Measurements will result in.
 type MetricType int
 
 const (
+	// Counter represents a positive change in value for a flush interval.
 	Counter MetricType = iota
+
+	// DerivedCounter represents a monotonically increasing counter value that
+	// is not supposed to be reset by the source.
 	DerivedCounter
+
+	// Gauge represents the value right now.
 	Gauge
 
-	// Currently unused, except in statsd parsing.
+	// Timer is currently unused, but exists for statsd parsing purposes.
 	Timer
 )
 
+// Measurement is a point in time value that is used to amend a metric.
 type Measurement struct {
-	Name      string
+	// Name is the metric to contribute to.
+	Name string
+
+	// Timestamp is the time at which the measurement was taken.
 	Timestamp time.Time
-	Type      MetricType
-	Value     float64
-	Sample    float32
-	Modifier  string
+
+	// Type is the type of the metric we wish to amend.
+	Type MetricType
+
+	// Value is the amount by which we will amend the metric.
+	// For Gauges, the amendment might be "replace".
+	Value float64
+
+	// Sample stores the sample rate that this measurement was taken at.
+	// For most applications, this should be set to 1.0
+	Sample float32
+
+	// Modifier allows gauges to be treated differently
+	//
+	// A value of "-" subtracts Value from the metric's previous value.
+	// A value of "+" adds Value to metric's previous value.  An empty
+	// value replaces the metric's value with Value.
+	Modifier string
 }
 
+// MetricSet provides a container for a set of metrics, and encodes
+// the rules for how metrics are updated given a Measurement.
 type MetricSet struct {
 	Counters     map[string]float64 `json:"counters,omitempty"`
 	Gauges       map[string]float64 `json:"gauges,omitempty"`
@@ -29,7 +56,13 @@ type MetricSet struct {
 	parent       *MetricSet
 }
 
-func NewMeasurementSet(parent *MetricSet) *MetricSet {
+// NewMetricSet constructs a MetricSet which can be used to turn
+// Measurements into metrics, that can be reported via a reporter.
+//
+// If a parent is given, it is expected to be the previously reported
+// MetricSet, in order to capture the change in metrics, for both
+// DerivedCounters, and modified Guages.
+func NewMetricSet(parent *MetricSet) *MetricSet {
 	return &MetricSet{
 		Counters:     make(map[string]float64),
 		Gauges:       make(map[string]float64),
@@ -38,6 +71,11 @@ func NewMeasurementSet(parent *MetricSet) *MetricSet {
 	}
 }
 
+// Update applies a Measurement to the MetricSet depending on its
+// Type.
+//
+// In cases where a Measurement for a Metric has a different Type than
+// was previously updated, a new Metric with that type will be created.
 func (ms *MetricSet) Update(m *Measurement) {
 	switch m.Type {
 	case Counter:
@@ -79,6 +117,8 @@ func (ms *MetricSet) Update(m *Measurement) {
 	}
 }
 
+// Snapshot returns a copy of the MetricSet, removing the reference to
+// the MetricSet's parent.
 func (ms *MetricSet) Snapshot() *MetricSet {
 	out := &MetricSet{
 		Counters:     make(map[string]float64),
@@ -98,6 +138,7 @@ func (ms *MetricSet) Snapshot() *MetricSet {
 	return out
 }
 
+// Len returns the cardinality of this set.
 func (ms *MetricSet) Len() int {
 	return len(ms.Counters) + len(ms.Gauges)
 }
